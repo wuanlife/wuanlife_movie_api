4d7574c3a23fb53f083dc93a874e07e698bc1725
Gtaker <459472218@qq.com>
2018-04-01 19:26:32 +0800

    修改返回格式

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -13,13 +13,7 @@ class TypeController extends Controller
         if (empty($base)) {
             return response(['error' => "获取分类信息失败"], 400);
         }
-        foreach ($base as $key => $value) {
-            $base[$key]['en_name'] = $base[$key]['type_id'];
-            $base[$key]['cn_name'] = $base[$key]['type_name'];
-            unset($base[$key]['type_id']);
-            unset($base[$key]['type_name']);
-        }
-        return $base;
+        return response(['type' => $base], 200);
     }
 
 }
