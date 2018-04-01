a1d359d41b37c92c96590eefe552d8ab50fa518a
Gtaker <459472218@qq.com>
2018-04-01 18:19:40 +0800

    增加数据表模型

---
+++ app/MoviesGenresDetails.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MoviesGenresDetails extends Model
+{
+    public $timestamps = false;
+    protected $table = 'movies_genres_details';
+}
