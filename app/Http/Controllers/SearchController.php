8820c0e526fcb377c95b164899e5218d08587280
Gtaker <459472218@qq.com>
2018-04-18 21:32:35 +0800

    更正搜索逻辑

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,16 +16,16 @@ class SearchController extends Controller
             $keywords = $request->input('q');
             $limit = $request->input('limit') ?? 20;
             $offset = $request->input('offset') ?? 0;
-            $base = Movies_base::where('title', 'like', $keywords . '%')->orwhere('digest', 'like',
-                $keywords . '%')->offset($offset)->limit($limit)->get();
+            $base = Movies_base::where('title', 'like', '%' . $keywords . '%')->orwhere('digest', 'like',
+                '%' . $keywords . '%')->offset($offset)->limit($limit)->get();
 
             foreach ($base as $key => $value) {
                 $base[$key]['poster'] = Movies_poster::where('id', '=', $base[$key]['id'])->get()[0]['url'];
                 $base[$key]['rating'] = Movies_rating::where('id', '=', $base[$key]['id'])->get()[0]['rating'];
             }
             $data['movies'] = $base;
-            $data['total'] = Movies_base::where('title', 'like', $keywords . '%')->orwhere('digest', 'like',
-                $keywords . '%')->count();
+            $data['total'] = Movies_base::where('title', 'like', '%' . $keywords . '%')->orwhere('digest', 'like',
+                '%' . $keywords . '%')->count();
 
             return response($data, 200);
         } else {
