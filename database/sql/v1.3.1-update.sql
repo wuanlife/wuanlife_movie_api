ad220c6d4f84d1f7286552e0a27b082cc6bc06c8
Gtaker <459472218@qq.com>
2018-04-26 15:46:35 +0800

    更新数据库结构文件组成

--- database/sql/v1.3.1-update.sql
+++ database/sql/v1.3.1-update.sql
@@ -1,9 +1,3 @@
-INSERT INTO movies_type_details VALUES
-(1,'电影'),(2,'国产/港台剧'),(3,'欧美剧'),(4,'日韩剧'),(5,'综艺');
-
-INSERT INTO resources_type_details VALUES
-(1,'在线'),(2,'磁力'),(3,'电驴'),(4,'网盘');
-
 -- 积分表
 CREATE TABLE IF NOT EXISTS scores
 (
