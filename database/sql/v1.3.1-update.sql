aa2877a6bcda630f06bf04cc80a357bee8c5f7dd
Gtaker <459472218@qq.com>
2018-04-26 15:39:38 +0800

    更新sql文件

--- database/sql/v1.3.1-update.sql
+++ database/sql/v1.3.1-update.sql
@@ -1,3 +1,10 @@
+INSERT INTO movies_type_details VALUES
+(1,'电影'),(2,'国产/港台剧'),(3,'欧美剧'),(4,'日韩剧'),(5,'综艺');
+
+INSERT INTO resources_type_details VALUES
+(1,'在线'),(2,'磁力'),(3,'电驴'),(4,'网盘');
+
+-- 积分表
 CREATE TABLE IF NOT EXISTS scores
 (
   user_id INT UNSIGNED NOT NULL
@@ -11,6 +18,7 @@ CREATE TABLE IF NOT EXISTS scores
   COLLATE = utf8_bin
   COMMENT = '用户积分表';
 
+-- 待审核资源表
 CREATE TABLE IF NOT EXISTS unreviewed_resources
 (
   resources_id INT UNSIGNED NOT NULL
