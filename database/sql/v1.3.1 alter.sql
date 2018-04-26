47e7eba0ee9dfd142a290b99d7a1c5662ce6103e
Gtaker <459472218@qq.com>
2018-04-26 10:34:23 +0800

    增加待审核资源表

--- database/sql/v1.3.1 alter.sql
+++ database/sql/v1.3.1 alter.sql
@@ -10,3 +10,15 @@ CREATE TABLE IF NOT EXISTS scores
   DEFAULT CHARSET = utf8
   COLLATE = utf8_bin
   COMMENT = '用户积分表';
+
+CREATE TABLE IF NOT EXISTS unreviewed_resources
+(
+  resources_id INT UNSIGNED NOT NULL
+  COMMENT '资源id',
+  PRIMARY KEY (resources_id)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_bin
+  COMMENT = '待审核资源表';
+
