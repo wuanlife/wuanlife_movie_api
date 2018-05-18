643fdff0b99d6ce8fd8a708ba4f30abb96d686c4
Gtaker <459472218@qq.com>
2018-05-18 17:12:28 +0800

    完善模型信息

--- app/Model/Resources/UnreviewedResources.php
+++ app/Model/Resources/UnreviewedResources.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class UnreviewedResources extends Model
 {
     public $timestamps = false;
+    protected $primaryKey = 'resources_id';
     protected $table = 'unreviewed_resources';
+    protected $fillable = [
+        'resources_id',
+    ];
 
     public function resource()
     {
-        return $this->hasOne('App\Model\Resources\Resource','resource_id','resources_id');
+        return $this->hasOne('App\Model\Resources\Resource', 'resource_id', 'resources_id');
     }
 }
