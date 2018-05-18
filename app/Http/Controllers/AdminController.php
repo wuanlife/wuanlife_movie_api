2383f841d8471e1e453f58ba9782ad2489d6997e
lwugit <jyh120@outlook.com>
2018-05-19 01:24:24 +0800

    初步完成M4 M5 M6

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,6 +11,15 @@ class AdminController extends Controller
     public function addAdmin()
     {
         // M4 新增管理员
+        $id = 1;
+
+        if(DB::table('users_auth')->where('id',$id)->update(array('auth'=>1)))
+        {
+            return response("新增成功",200);
+        }
+        else{
+            return response(['error'=>"非法请求"],400);
+        }
 
     }
 
@@ -25,16 +34,20 @@ class AdminController extends Controller
         else{
             return response(['error'=>"非法请求"],400);
         }
-
-
-
-
     }
 
     public function listAdmin()
     {
         //获取管理员列表
-        $a =DB::table('users_auth')->get();
-        return $a;
+        $r =DB::table('users_auth')->get();
+        $re['admins'] = json_decode(json_encode($r),true);
+//        print_r($re['admins'][0]);
+        foreach ($re['admins'] as $key => $value) {
+            $api_url = env('OIDC_SERVER_GET_USER_INFO_API') . '/api/app/users/' . $re['admins'][$key]['id'];
+            $response = file_get_contents($api_url);
+            $user = json_decode($response);
+            $value['name'] = $user->name;
+        }
+        return response($re, 200);
     }
 }
