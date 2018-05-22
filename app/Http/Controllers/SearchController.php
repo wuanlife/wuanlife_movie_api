e4e86c251c888bb565556881a49becff398b8c40
Gtaker <459472218@qq.com>
2018-05-22 18:30:31 +0800

    更正错误的大小写

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movies\{
-    Moviesposter, MoviesRating, MoviesBase
+    MoviesPoster, MoviesRating, MoviesBase
 };
 use Illuminate\Http\Request;
 
