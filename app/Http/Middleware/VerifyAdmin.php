bbde6d50e8cfddb456449d2648eb59dc1b3aca6b
Gtaker <459472218@qq.com>
2018-05-22 19:48:28 +0800

    添加 try catch 处理机制

--- app/Http/Middleware/VerifyAdmin.php
+++ app/Http/Middleware/VerifyAdmin.php
@@ -18,27 +18,31 @@ class VerifyAdmin
      */
     public function handle($request, Closure $next)
     {
-        $id_token = $request->header('ID-Token');
-        $id_token = json_decode(
-            base64_decode(
-                explode('.', $id_token)[1]
-            )
-        );
-        $user_id = $id_token->uid;
+        try {
+            $id_token = $request->header('ID-Token');
+            $id_token = json_decode(
+                base64_decode(
+                    explode('.', $id_token)[1]
+                )
+            );
+            $user_id = $id_token->uid;
 
-        $res = DB::table('users_auth')
-            ->join('auth_detail', 'users_auth.auth', 'auth_detail.id')
-            ->where('users_auth.id', $user_id)
-            ->whereIn('identity', [
-                '管理员',
-                '最高管理员',
-            ])
-            ->select('users_auth.auth')
-            ->count();
-        if (!$res) {
-            return response(['error' => '权限不足，需要管理员权限'], 403);
+            $res = DB::table('users_auth')
+                ->join('auth_detail', 'users_auth.auth', 'auth_detail.id')
+                ->where('users_auth.id', $user_id)
+                ->whereIn('identity', [
+                    '管理员',
+                    '最高管理员',
+                ])
+                ->select('users_auth.auth')
+                ->count();
+            if (!$res) {
+                return response(['error' => '权限不足，需要管理员权限'], 403);
+            }
+        } catch (\Exception $e) {
+
+            return response(['error' => $e->getMessage()], 400);
         }
-
         return $next($request);
     }
 }
