b7161949ba604e8b0f5d102ecacac3ec698790c2
Gtaker <459472218@qq.com>
2018-05-22 17:46:29 +0800

    构造静态方法，透明的构造访问内部 API 的请求

--- app/Http/Controllers/Builder.php
+++ app/Http/Controllers/Builder.php
@@ -2,11 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Hash;
 
 class Builder extends Controller
 {
-    public static function queryToken()
+    /**
+     * 构造内部通信请求
+     * @param $url
+     * @param string $method
+     * @param array $header
+     * @param array $param
+     * @return array|\Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function requestInnerApi($url, $method = 'GET', $header = [], $param = [])
+    {
+        $client = new Client(['base_uri' => env('OIDC_SERVER')]);
+        $params = array_merge(self::getParam(), $param);
+        $response = $client->request($method, $url, [
+                'headers' => $header,
+                'json' => $params,
+            ]
+        );
+
+        return [
+            'status_code' => $response->getStatusCode(),
+            'contents' => $response->getBody()->getContents(),
+        ];
+    }
+
+    /**
+     * 构造内部请求时验证所需的信息
+     * @return array
+     */
+    private static function getParam()
     {
         $app = env('APP_NAME');
         $secret = env('SECRET');
@@ -17,8 +47,11 @@ class Builder extends Controller
             ]
         );
         $key = Hash::make($info . $secret);
-        $query = "app={$app}&info={$info}&key={$key}";
 
-        return $query;
+        return [
+            'app' => $app,
+            'info' => $info,
+            'key' => $key,
+        ];
     }
 }
