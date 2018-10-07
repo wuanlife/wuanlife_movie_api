1ca4fa7c98a0fb8d4d9cd01af23f8a47a7e8c30d
AilonHuang <529678737@qq.com>
2018-10-07 15:02:08 +0800

    后台登陆

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Builder;
 use App\Http\Controllers\Controller;
 use App\Models\Users\UsersAuth;
-use function foo\func;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -18,8 +16,7 @@ class LoginController extends Controller
     /**
      * 登陆
      * @param Request $request
-     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
     {
@@ -30,31 +27,41 @@ class LoginController extends Controller
         ]);
 
         // 调用 oidc 登陆
-        $login = Builder::requestInnerApi(
-            env('OIDC_SERVER'),
-            "/api/users/login",
-            'POST',
-            [],
-            $credentials
-        );
-        $content = json_decode($login['contents'], true);
-        // 判断登陆结果
-        if (200 !== $login['status_code']) {
-            $request->session()->flash('danger', $content['error']);
-            return redirect()->back()->withInput();
+        {
+            $result = Http::request(env('OIDC_SERVER'), '/api/users/login', 'POST', [], $credentials);
+            $content = json_decode($result['contents'], true);
+            // 判断登陆结果
+            if (200 !== $result['status_code']) {
+                session()->flash('danger', $content['error']);
+                return redirect()->back()->withInput();
+            }
+
+            $id_token = $content['ID-Token'];
         }
 
         // 验证权限
-        $id_token = $content['ID-Token'];
-        $id_token = json_decode(base64_decode(explode('.', $id_token)[1]));
-        $user_id = $id_token->uid;
+        $user_info = json_decode(base64_decode(explode('.', $id_token)[1]));
+        $user_id = $user_info->uid;
 
         if (!UsersAuth::checkAuth($user_id)) {
             $request->session()->flash('danger', '权限不足');
             return redirect()->back()->withInput();
         }
 
-        $request->session()->put('wuan', ['ID-Token' => $content['ID-Token']]);
+        // 获取 Access-Token
+        {
+            $header = ['ID-Token' => $id_token];
+            $params = ['scope' => 1];
+            $result = Http::request(env('OIDC_SERVER'), 'api/auth', 'POST', $header, $params);
+            $content = json_decode($result['contents'], true);
+            if (200 !== $result['status_code']) {
+                session()->flash('danger', $content['error']);
+                return redirect()->back()->withInput();
+            }
+            $access_token =  $content['Access-Token'];
+        }
+
+        $request->session()->put('wuan', ['ID-Token' => $id_token, 'Access-Token' => $access_token]);
         session()->flash('success', '欢迎回来! ');
         return redirect()->intended(route('admin.resources'));
     }
